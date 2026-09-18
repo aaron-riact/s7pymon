krisl/busfactor

simplify: remove dead flash code, _region_refresh takes indices, add _rebuild_and_refresh helper
Remove _changed_abs_offsets property (lossy, unused in production), _abs_from_keys (unused), and dead 'changed' assignment in _rebuild_lines.

_region_refresh now takes resolved line indices instead of keys — eliminates double _lines_for_offsets call at every call site.

New _rebuild_and_refresh(keys) helper replaces the rebuild+refresh two-liner in both set_data and set_selected_offsets.

Remove dead 'if fs:' guard in _build_hex_line flash branches (_flash_style_for never returns None when flash_key is in _flash_cycles).  Tests updated to use _changed_flash_keys.

src/s7pymon/app.py
@@ -126,11 +126,6 @@ class HexDumpDisplay(Static):
         self._rebuild_lines()
 
     @property
-    def _changed_abs_offsets(self) -> set[int]:
-        """Legacy — absolute offsets across all groups (lossy for colliding offsets)."""
-        return {int(k.split(":", 1)[1]) for k in self._flash_cycles}
-
-    @property
     def _changed_flash_keys(self) -> set[str]:
         return set(self._flash_cycles.keys())
 
@@ -165,17 +160,11 @@ class HexDumpDisplay(Static):
             return
         self._selected_abs_offsets[group_label] = offsets
         affected_abs = (old or set()) | offsets
-        affected_keys = {self._flash_key(group_label, o) for o in affected_abs}
-        self._rebuild_some_lines(self._lines_for_offsets(affected_keys))
-        self._region_refresh(affected_keys)
+        self._rebuild_and_refresh({self._flash_key(group_label, o) for o in affected_abs})
 
     @staticmethod
     def _flash_key(label: str, abs_offset: int) -> str:
         return f"{label}:{abs_offset}"
-
-    @staticmethod
-    def _abs_from_keys(keys: set[str]) -> set[int]:
-        return {int(k.split(":", 1)[1]) for k in keys}
 
     def set_data(
         self,
@@ -202,9 +191,7 @@ class HexDumpDisplay(Static):
             self._rebuild_lines()
             self.refresh(layout=needs_layout)
         elif new_changed or flash_affected:
-            affected_keys = new_changed | flash_affected
-            self._rebuild_some_lines(self._lines_for_offsets(affected_keys))
-            self._region_refresh(affected_keys)
+            self._rebuild_and_refresh(new_changed | flash_affected)
 
     # -- Reactives ------------------------------------------------------------
 
@@ -293,14 +280,9 @@ class HexDumpDisplay(Static):
 
     # -- Helpers --------------------------------------------------------------
 
-    def _region_refresh(self, keys: set[str]) -> None:
-        """Refresh contiguous blocks of affected lines only.
-
-        *keys* are qualified ``"{label}:{abs_offset}"`` — only lines
-        whose group matches the key label are included, so groups
-        with the same starting offset don't collide.
-        """
-        raw = sorted(self._lines_for_offsets(keys))
+    def _region_refresh(self, indices: set[int]) -> None:
+        """Refresh contiguous blocks of affected lines only."""
+        raw = sorted(indices)
         if not raw:
             return
         w = self.size.width or 80
@@ -314,6 +296,12 @@ class HexDumpDisplay(Static):
                 start = idx
                 end = idx
         self.refresh(Region(0, start, w, end - start + 1))
+
+    def _rebuild_and_refresh(self, keys: set[str]) -> None:
+        """Rebuild and refresh lines affected by *keys* (qualified ``"{label}:{abs}"``)."""
+        indices = self._lines_for_offsets(keys)
+        self._rebuild_some_lines(indices)
+        self._region_refresh(indices)
 
     def _lines_for_offsets(self, keys: set[str]) -> set[int]:
         """Return indices of hex lines whose (group, byte range) overlaps *keys*.
@@ -377,18 +365,10 @@ class HexDumpDisplay(Static):
 
             if byte_abs in group_selected and flash_key in self._flash_cycles:
                 cycles = self._flash_cycles[flash_key]
-                fs = self._flash_style_for(cycles)
-                if fs:
-                    style = Style.parse(f"bold reverse {fs}")
-                else:
-                    style = Style.parse("bold reverse")
+                style = Style.parse(f"bold reverse {self._flash_style_for(cycles)}")
             elif flash_key in self._flash_cycles:
                 cycles = self._flash_cycles[flash_key]
-                fs = self._flash_style_for(cycles)
-                if fs:
-                    style = Style.parse(fs)
-                else:
-                    style = Style()
+                style = Style.parse(self._flash_style_for(cycles))
             elif byte_abs in group_selected:
                 style = Style.parse("bold reverse")
             elif not interesting:
@@ -437,7 +417,6 @@ class HexDumpDisplay(Static):
 
         for gidx, (label, data, start) in enumerate(self._group_data):
             group_selected = self._selected_abs_offsets.get(label, set())
-            changed = self._changed_abs_offsets
             group_rendered = False
 
             for i in range(0, len(data), 16):

aaron-riact/s7pymon

src/s7pymon/app.py
@@ -131,11 +131,6 @@ class HexDumpDisplay(Static):
         self._rebuild_lines()
 
     @property
-    def _changed_abs_offsets(self) -> set[int]:
-        """Legacy — absolute offsets across all groups (lossy for colliding offsets)."""
-        return {offset for _, offset in self._flash_cycles}
-
-    @property
     def _changed_flash_keys(self) -> set[_ByteKey]:
         return set(self._flash_cycles)
 
@@ -170,9 +165,7 @@ class HexDumpDisplay(Static):
             return
         self._selected_abs_offsets[group_label] = offsets
         affected_abs = (old or set()) | offsets
-        affected_keys = {(group_label, o) for o in affected_abs}
-        self._rebuild_some_lines(self._lines_for_offsets(affected_keys))
-        self._region_refresh(affected_keys)
+        self._rebuild_and_refresh({(group_label, o) for o in affected_abs})
 
     def set_data(
         self,
@@ -197,9 +190,7 @@ class HexDumpDisplay(Static):
             self._rebuild_lines()
             self.refresh(layout=needs_layout)
         elif new_changed or flash_affected:
-            affected_keys = new_changed | flash_affected
-            self._rebuild_some_lines(self._lines_for_offsets(affected_keys))
-            self._region_refresh(affected_keys)
+            self._rebuild_and_refresh(new_changed | flash_affected)
 
     # -- Reactives ------------------------------------------------------------
 
@@ -288,9 +279,9 @@ class HexDumpDisplay(Static):
 
     # -- Helpers --------------------------------------------------------------
 
-    def _region_refresh(self, keys: set[_ByteKey]) -> None:
+    def _region_refresh(self, indices: set[int]) -> None:
         """Refresh contiguous blocks of affected lines only."""
-        raw = sorted(self._lines_for_offsets(keys))
+        raw = sorted(indices)
         if not raw:
             return
         w = self.size.width or 80
@@ -304,6 +295,12 @@ class HexDumpDisplay(Static):
                 start = idx
                 end = idx
         self.refresh(Region(0, start, w, end - start + 1))
+
+    def _rebuild_and_refresh(self, keys: set[_ByteKey]) -> None:
+        """Rebuild and refresh the lines holding any of the bytes in *keys*."""
+        indices = self._lines_for_offsets(keys)
+        self._rebuild_some_lines(indices)
+        self._region_refresh(indices)
 
     def _lines_for_offsets(self, keys: set[_ByteKey]) -> set[int]:
         """Return indices of hex lines holding any of the bytes in *keys*.
@@ -361,18 +358,10 @@ class HexDumpDisplay(Static):
 
             if byte_abs in group_selected and flash_key in self._flash_cycles:
                 cycles = self._flash_cycles[flash_key]
-                fs = self._flash_style_for(cycles)
-                if fs:
-                    style = Style.parse(f"bold reverse {fs}")
-                else:
-                    style = Style.parse("bold reverse")
+                style = Style.parse(f"bold reverse {self._flash_style_for(cycles)}")
             elif flash_key in self._flash_cycles:
                 cycles = self._flash_cycles[flash_key]
-                fs = self._flash_style_for(cycles)
-                if fs:
-                    style = Style.parse(fs)
-                else:
-                    style = Style()
+                style = Style.parse(self._flash_style_for(cycles))
             elif byte_abs in group_selected:
                 style = Style.parse("bold reverse")
             elif not interesting:
@@ -421,7 +410,6 @@ class HexDumpDisplay(Static):
 
         for gidx, (label, data, start) in enumerate(self._group_data):
             group_selected = self._selected_abs_offsets.get(label, set())
-            changed = self._changed_abs_offsets
             group_rendered = False
 
             for i in range(0, len(data), 16):