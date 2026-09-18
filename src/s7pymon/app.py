krisl/busfactor

Allow follow rule source to reference addresses not defined as variables
Two changes in _apply_rules:
1. Populate current_values by label (var.label -> formatted value) so follow rules can reference a variable by its label instead of its spec.
2. For follow rules whose source is not in current_values, parse the source as a variable spec and extract the value directly from the read buffer. This allows rules like 'follow: EIP.Input.Byte0' even when no variable is defined at that address.

src/s7pymon/app.py
@@ -505,12 +505,26 @@ class S7MonitorApp(App):
             try:
                 value = extract_value(var, data, data_start)
                 current_values[var.spec] = var.format_value(value)
+                if var.label:
+                    current_values[var.label] = current_values[var.spec]
             except Exception:
                 pass
-        import sys
-        print(f"[app] _apply_rules with {len(current_values)} values", file=sys.stderr, flush=True)
-        if self._rules_engine._verbose:
-            print(f"[app] _apply_rules: values={current_values}", file=sys.stderr, flush=True)
+
+        # Resolve follow rule sources not backed by a defined variable
+        for rule in self._rules_engine.rules:
+            if not hasattr(rule, 'source') or rule.source in current_values:
+                continue
+            try:
+                var = S7Variable.parse(rule.source)
+                key = str(var.source)
+                entry = buffers.get(key)
+                if entry is not None:
+                    data, data_start = entry
+                    value = extract_value(var, data, data_start)
+                    current_values[rule.source] = var.format_value(value)
+            except Exception:
+                pass
+
         self._rules_engine.apply(self._connection, current_values)
 
     def trigger_pulse(self, target: str) -> None:

aaron-riact/s7pymon

src/s7pymon/app.py
@@ -505,12 +505,26 @@ class S7MonitorApp(App):
             try:
                 value = extract_value(var, data, data_start)
                 current_values[var.spec] = var.format_value(value)
+                if var.label:
+                    current_values[var.label] = current_values[var.spec]
             except Exception:
                 pass
-        import sys
-        print(f"[app] _apply_rules with {len(current_values)} values", file=sys.stderr, flush=True)
-        if self._rules_engine._verbose:
-            print(f"[app] _apply_rules: values={current_values}", file=sys.stderr, flush=True)
+
+        # Resolve follow rule sources not backed by a defined variable
+        for rule in self._rules_engine.rules:
+            if not hasattr(rule, 'source') or rule.source in current_values:
+                continue
+            try:
+                var = Variable.parse(rule.source)
+                key = str(var.source)
+                entry = buffers.get(key)
+                if entry is not None:
+                    data, data_start = entry
+                    value = extract_value(var, data, data_start)
+                    current_values[rule.source] = var.format_value(value)
+            except Exception:
+                pass
+
         self._rules_engine.apply(self._connection, current_values)
 
     def trigger_pulse(self, target: str) -> None: